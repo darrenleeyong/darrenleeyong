69a7da99ae18fc30a111472f61c2c9c7b1af7de3
darstars <darstars@gmail.com>
2016-10-23 10:07:14 +0800

    Added descriptions
    fixed some small bugs. testing for responsiveness on mobile devices
    (worked differently on console).

--- js/main.js
+++ js/main.js
@@ -70,18 +70,6 @@
 
 	}
 
-	// Superfish Sub Menu Click ( Mobiles/Tablets )
-	var mobileClickSubMenus = function() {
-
-		$('body').on('click', '.fh5co-sub-ddown', function(event) {
-			event.preventDefault();
-			var $this = $(this),
-				li = $this.closest('li');
-			li.find('> .fh5co-sub-menu').slideToggle(200);
-		});
-
-	};
-
 
 	// Animations
 
