c06e2ebe24983ea0b453877fe794ddfae53a355a
darrenleeyong <darstars@gmail.com>
2018-01-25 17:49:14 +0800

    gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var pump = require('pump');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
 var run = require('gulp-run');
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminMozjpeg = require('imagemin-mozjpeg'); //need to run 'brew install libpng'
+var imageminGiflossy = require('imagemin-giflossy');
+ 
 
 gulp.task('compress-js', function (cb) {
   pump([
@@ -27,4 +33,45 @@ gulp.task('font', function(){
     .pipe(gulp.dest('dist/fonts'))
 });
 
-gulp.task('default', [ 'compress-js', 'css', 'font']); 
+//compress all images
+gulp.task('imagemin', function() {
+    return gulp.src(['images/**/*.{gif,png,jpg}'])
+        .pipe(imagemin([
+            //png
+            imageminPngquant({
+                speed: 1,
+                quality: 98 //lossy settings
+            }),
+            imageminZopfli({
+                more: true
+            }),
+            //gif
+            // imagemin.gifsicle({
+            //     interlaced: true,
+            //     optimizationLevel: 3
+            // }),
+            //gif very light lossy, use only one of gifsicle or Giflossy
+            imageminGiflossy({
+                optimizationLevel: 3,
+                optimize: 3, //keep-empty: Preserve empty transparent frames
+                lossy: 2
+            }),
+            //svg
+            imagemin.svgo({
+                plugins: [{
+                    removeViewBox: false
+                }]
+            }),
+            //jpg lossless
+            imagemin.jpegtran({
+                progressive: true
+            }),
+            //jpg very light lossy, use vs jpegtran
+            imageminMozjpeg({
+                quality: 90
+            })
+        ]))
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('default', [ 'compress-js', 'css', 'font', 'imagemin']); 
